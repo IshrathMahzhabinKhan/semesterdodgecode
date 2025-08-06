51e08590dd873ddd1b46e95aaa98d9f9acdf96fe
IshrathMahzhabinKhan <officialishrath30@gmail.com>
2025-08-06 20:18:21 +0600

    Add files via upload
    Second commit

--- semesterdodge/src/semesterdodge/GamePanel.java
+++ semesterdodge/src/semesterdodge/GamePanel.java
@@ -4,13 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
     Image background;
+    Timer timer;
+    Player player;
 
     public GamePanel(GameFrame frame) {
         setLayout(null);
+        setFocusable(true);
+        requestFocusInWindow();
+        addKeyListener(this);
 
         background = AssetLoader.load("res/background.png");
+
+        player = new Player(); 
 
         JRadioButton exitButton = new JRadioButton("Exit Game");
         exitButton.setBounds(650, 20, 120, 30);
@@ -23,16 +30,42 @@ public class GamePanel extends JPanel {
             }
         });
         add(exitButton);
+
+        timer = new Timer(20, this); 
+        timer.start();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        
+
         if (background != null) {
             g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
         } else {
             g.setColor(Color.BLACK);
             g.fillRect(0, 0, getWidth(), getHeight());
         }
+
+        // Draw player
+        if (player != null) {
+            player.draw(g);
+        }
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        player.update(); 
+        repaint();       
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            player.jump(); 
+        }
+    }
+
+    @Override public void keyReleased(KeyEvent e) {}
+    @Override public void keyTyped(KeyEvent e) {}
 }
---
+++ semesterdodge/src/semesterdodge/Player.java
@@ -0,0 +1,56 @@
+package semesterdodge;
+
+import java.awt.*;
+
+public class Player {
+    int x = 100;
+    int y;
+    int width = 50, height = 50;
+    int velocityY = 0;
+    boolean jumping = false;
+
+    final int ground = 240;
+    final int gravity = 1;
+
+    Image[] runFrames;
+    Image jumpImg;
+    int frameCount = 0;
+
+    public Player() {
+        y = ground - height;  
+
+        runFrames = new Image[] {
+            AssetLoader.load("res/player/run1.png"),
+            AssetLoader.load("res/player/run2.png"),
+            AssetLoader.load("res/player/run3.png")
+        };
+        jumpImg = AssetLoader.load("res/player/jump.png");
+    }
+
+    public void jump() {
+        if (!jumping) {
+            velocityY = -15;
+            jumping = true;
+        }
+    }
+
+    public void update() {
+        y += velocityY;
+        velocityY += gravity;
+
+        if (y + height >= ground) {
+            y = ground - height; // Reset on ground
+            jumping = false;
+            velocityY = 0; // reset velocity when on ground
+        }
+    }
+
+    public void draw(Graphics g) {
+        Image img = jumping ? jumpImg : runFrames[(frameCount++ / 10) % runFrames.length];
+        g.drawImage(img, x, y, width, height, null);
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
+}
